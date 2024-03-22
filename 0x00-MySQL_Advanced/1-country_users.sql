df16eaa703b4dba09a81233e90b37b425d3c242c
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-03-22 15:38:21 +0300

    added default us

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,9 @@
--- Creates a table 
+-- Creates a table
+
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR (255) NOT NULL UNIQUE,
     name VARCHAR (255),
-    country ENUM('US', 'CO', 'TN') NOT NULL,
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
 );
