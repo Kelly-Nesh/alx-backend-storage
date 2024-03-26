8dd31e5be9a7a54c1509e7c31d6a43e319f58b80
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-03-26 22:06:20 +0300

    12-log_stats.py

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Log nginx stats"""
+from pymongo import MongoClient
+
+
+if __name__ == '__main__':
+    client = MongoClient()
+    db = client.logs
+    coll = db['nginx']
+
+    print("{} logs".format(coll.count_documents({})))
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for m in methods:
+        print("\tmethod {}: {}".format(m, coll.count_documents({'method': m})))
+    print("{} status check".format(coll.count_documents({'path': '/status'})))
