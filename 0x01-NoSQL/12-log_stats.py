96c14ba1eb044c7cdae74883cb07f73d3bfa06f3
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-03-26 22:12:12 +0300

    added if name is main

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,14 +3,14 @@
 from pymongo import MongoClient
 
 
-client = MongoClient()
-db = client.logs
-coll = db['nginx']
+if __name__ == '__main__':
+    client = MongoClient()
+    db = client.logs
+    coll = db['nginx']
 
-
-print("{} logs".format(coll.count_documents({})))
-print("Methods:")
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-for m in methods:
-    print("\tmethod {}: {}".format(m, coll.count_documents({'method': m})))
-print("{} status check".format(coll.count_documents({'path': '/status'})))
+    print("{} logs".format(coll.count_documents({})))
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for m in methods:
+        print("\tmethod {}: {}".format(m, coll.count_documents({'method': m})))
+    print("{} status check".format(coll.count_documents({'path': '/status'})))
