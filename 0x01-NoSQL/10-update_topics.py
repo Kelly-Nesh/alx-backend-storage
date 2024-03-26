c863aaa0d80eb1206c8d928e4cb8cf39655173a8
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-03-26 20:54:56 +0300

    update many

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -4,5 +4,5 @@
 
 def update_topics(mongo_collection, name, topics):
     """Changes all topics of a school document based on the name"""
-    doc = mongo_collection.update_one(
+    doc = mongo_collection.update_many(
         {'name': name}, {'$set': {'topics': topics}})
