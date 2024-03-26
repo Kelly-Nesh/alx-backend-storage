6e1ae22d977fe7d577ca8874104e9c8e5ce1845a
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-03-26 19:59:54 +0300

    check empty kwargs

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -4,6 +4,8 @@
 
 def insert_school(mongo_collection, **kwargs):
     """Inserts a new document in a collection based on kwargs"""
+    if not kwargs:
+        return ''
     doc = mongo_collection.insert_one(kwargs)
     return str(doc.inserted_id)
 
