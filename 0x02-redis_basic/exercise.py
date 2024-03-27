e4741d092a0ae82b32b6743420cf4032406d0863
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-03-27 21:30:08 +0300

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-"""Writing strings to Redis"""
+"""Writing strings to Redis
+Reading from Redis and recovering original type
+"""
 import redis
 import uuid
 from typing import Union
@@ -18,3 +20,24 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn=None):
+        """Takes a key argument and returns the data stored in the key.
+        fn: callable will be used to convert the data
+            back to the desired format."""
+        if fn:
+            return fn(self._redis.get(key))
+        return self._redis.get(key)
+
+
+cache = Cache()
+
+TEST_CASES = {
+    b"foo": None,
+    123: int,
+    "bar": lambda d: d.decode("utf-8")
+}
+
+for value, fn in TEST_CASES.items():
+    key = cache.store(value)
+    assert cache.get(key, fn=fn) == value
